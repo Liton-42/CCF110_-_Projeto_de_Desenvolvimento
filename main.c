f9bd07cfeab7d1ded49a8b6e80b91ab235e31df5
Liton-42 <liton42@tutanota.com>
2023-10-23 23:03:37 -0300

    Implementação de matrizes, adição da possibilidade de criar voos e atribuir diferentes números de assentos.

--- main.c
+++ main.c
@@ -5,189 +5,155 @@
 int main(){
     setlocale(LC_ALL, "Portuguese");
 
-    int nVoo, selectVaga, i, consulVoo, reserVoo, cancelVoo; //nVoo = 3;
+    int i, j;
 
     //Voos disponiveis
-    int voo[] = {42, 77, 214};
+    int nVoo;
 
-    //Vagas totais nos voos
-    int assenTemp;
+    printf("Informe o número de voos disponíveis: ");
+    scanf("%d", &nVoo);
 
-    assenTemp = 411;
-    int vagaVoo042[assenTemp], vagaVoo077[assenTemp], vagaVoo214[assenTemp], contVaga, reserAssen,
-        cancelAssen;
+    //Cadastro dos voos
+    int idVoo[nVoo];
 
-    contVaga = 0;
+    printf("\n");
+    for(i = 0; i < nVoo; i++){
+        printf("Cadestre o %dº voo: ", i + 1);
+        scanf("%d", &idVoo[i]);
+    }
 
-    for(i = 0; i < assenTemp; i++){
-        vagaVoo042[i] = 0;//rand()%2;
-        vagaVoo077[i] = 0;//rand()%2;
-        vagaVoo214[i] = 0;//rand()%2;
+    //Vagas totais no aeroporto
+    int assenMax;
+
+    printf("\nInforme a capacidade máxima de assentos suportada pelo aeroporto: ");
+    scanf("%d", &assenMax);
+
+    int ocupMax[nVoo][assenMax + 1];
+
+    //Liberando todas as poltronas
+    for(i = 0; i < nVoo; i++){
+        for(j = 0; j < assenMax + 1; j++){
+            ocupMax[i][j] = 0;
+        }
+    }
+
+    //Vagas especificas dos aviões
+    int ocupMaxVoo[nVoo];
+
+    printf("\n");
+    for(i = 0; i < nVoo; i++){
+        printf("Total de assentos no Voo %d: ", idVoo[i]);
+        scanf("%d", &ocupMaxVoo[i]);
     }
 
     //Operando poltronas
+    int select, contVaga, consulVoo, reserVoo, reserAssen, cancelVoo, cancelAssen, indiceVoo;
+
+    contVaga = 0;
+
     printf("Informe a operação que deseja realizar:\n");
     printf(" 1. Consultar vagas\n");
     printf(" 2. Reservar assento\n");
     printf(" 3. Cancelar reserva\n");
     printf(" 4. Sair\n");
 
-    scanf("%d", &selectVaga);
+    scanf("%d", &select);
 
-    switch (selectVaga){
+    switch (select){
         case 1:
             printf("\n");
-            for(i = 0; i < 3; i++){
-                printf("Voo %d\n", voo[i]);
+            for(i = 0; i < nVoo; i++){
+                printf("Voo %d\n", idVoo[i]);
             }
             
             printf("\nInforme o voo para consultar as poltronas vagas (para encerrar digite 0): ");
             scanf("%d", &consulVoo);
 
-            if(consulVoo == voo[0]){
-                for(i = 1; i < assenTemp; i++){
-                    if(vagaVoo042[i] == 0){
-                        printf("  Poltrona %d\n", i);
-                        contVaga++;
-                    }
+            for(i = 0; i < nVoo; i++){
+                if(consulVoo == idVoo[i]){
+                    indiceVoo = i;
                 }
-
-                printf("Quantidade de poltronas livres: %d", contVaga);
             }
-            else if(consulVoo == voo[1]){
-                for(i = 1; i < assenTemp; i++){
-                    if(vagaVoo077[i] == 0){
-                        printf("  Poltrona %d\n", i);
-                        contVaga++;
-                    }
+
+            for(j = 1; j < ocupMaxVoo[indiceVoo] + 1; j++){
+                if(ocupMax[indiceVoo][j] == 0){
+                    contVaga++;
                 }
+            }
 
-                printf("Quantidade de poltronas livres: %d", contVaga);
-            }
-            else if(consulVoo == voo[2]){
-                for(i = 1; i < assenTemp; i++){
-                    if(vagaVoo214[i] == 0){
-                        printf("  Poltrona %d\n", i);
-                        contVaga++;
-                    }
-                }
-
-                printf("Quantidade de poltronas livres: %d", contVaga);
-            }
-            else if(consulVoo == 0){
-                break;
-            }
-            else{
-                printf("Informe um valor válido: ");
-                scanf("%d", &consulVoo);
-            }
+            printf("Quantidade de poltronas livres: %d", contVaga);
+            
             break;
         
         case 2:
             printf("\n");
-            for(i = 0; i < 3; i++){
-                printf("Voo %d\n", voo[i]);
+            for(i = 0; i < nVoo; i++){
+                printf("Voo %d\n", idVoo[i]);
             }
             
             printf("\nInforme o voo em que se realizara a reserva (para encerrar digite 0): ");
             scanf("%d", &reserVoo);
+
+            for(i = 0; i < nVoo; i++){
+                if(reserVoo == idVoo[i]){
+                    indiceVoo = i;
+                }
+            }
+
+            printf("\nInforme a poltrona que será reservada (para encerrar digite 0): ");
+            scanf("%d", &reserAssen);
             
-            if(reserVoo == voo[0]){
-                printf("\nInforme a poltrona que sera reservada (para encerrar digite 0 ou um valor acima da capacidade máxima): ");
+            if(reserAssen > ocupMaxVoo[indiceVoo]){
+                printf("\nA capacidade máxima do Voo %d é %d", idVoo[indiceVoo], ocupMaxVoo[indiceVoo]);
+                printf("\nInforme a uma poltrona valida (para encerrar digite 0): ");
                 scanf("%d", &reserAssen);
-                
-                while(reserAssen > 0){
-                    vagaVoo042[reserAssen] = 1;
+            }
 
-                    printf("\nInforme a poltrona que sera reservada (para encerrar digite 0 ou um valor acima da capacidade máxima): ");
+            while(reserAssen > 0){
+                ocupMax[indiceVoo][reserAssen] = 1;
+
+                printf("\nInforme a poltrona que será reservada (para encerrar digite 0): ");
+                scanf("%d", &reserAssen);
+
+                if(reserAssen > ocupMaxVoo[indiceVoo]){
+                    printf("\nA capacidade máxima do Voo %d é %d", idVoo[indiceVoo], ocupMaxVoo[indiceVoo]);
+                    printf("\nInforme a uma poltrona valida (para encerrar digite 0): ");
                     scanf("%d", &reserAssen);
                 }
             }
-            else if(reserVoo == voo[1]){
-                printf("\nInforme as poltronas que sera reservadas (para encerrar digite 0 ou um valor acima da capacidade máxima): ");
-                scanf("%d", &reserAssen);
-                
-                while(reserAssen > 0){
-                    vagaVoo077[reserAssen] = 1;
 
-                    printf("\nInforme a poltrona que sera reservada (para encerrar digite 0 ou um valor acima da capacidade máxima): ");
-                    scanf("%d", &reserAssen);
-                }
-            }
-            else if(reserVoo == voo[2]){
-                printf("\nInforme as poltronas que sera reservadas (para encerrar digite 0 ou um valor acima da capacidade máxima): ");
-                scanf("%d", &reserAssen);
-                
-                while(reserAssen > 0){
-                    vagaVoo214[reserAssen] = 1;
-
-                    printf("\nInforme a poltrona que sera reservada (para encerrar digite 0 ou um valor acima da capacidade máxima): ");
-                    scanf("%d", &reserAssen);
-                }
-            }
-            else if(reserVoo == 0){
-                break;
-            }
-            else{
-                printf("Informe um valor válido: ");
-                scanf("%d", &reserVoo);
-            }
             break;
 
         case 3:
             printf("\n");
-            for(i = 0; i < 3; i++){
-                printf("Voo %d\n", voo[i]);
+            for(i = 0; i < nVoo; i++){
+                printf("Voo %d\n", idVoo[i]);
             }
             
-            printf("\nInforme o voo em que se realizara a reserva (para encerrar digite 0): ");
+            printf("\nInforme o voo em que a reserva será cancelada (para encerrar digite 0): ");
             scanf("%d", &cancelVoo);
-            
-            if(cancelVoo == voo[0]){
-                printf("\nInforme a poltrona que sera reservada (para encerrar digite 0 ou um valor acima da capacidade máxima): ");
-                scanf("%d", &cancelAssen);
-                
-                while(cancelAssen > 0){
-                    vagaVoo042[cancelAssen] = 0;
 
-                    printf("\nInforme a poltrona que sera reservada (para encerrar digite 0 ou um valor acima da capacidade máxima): ");
-                    scanf("%d", &cancelAssen);
+            for(i = 0; i < nVoo; i++){
+                if(cancelVoo == idVoo[i]){
+                    indiceVoo = i;
                 }
             }
-            else if(cancelVoo == voo[1]){
-                printf("\nInforme as poltronas que sera reservadas (para encerrar digite 0 ou um valor acima da capacidade máxima): ");
-                scanf("%d", &cancelAssen);
-                
-                while(cancelAssen > 0){
-                    vagaVoo077[cancelAssen] = 0;
 
-                    printf("\nInforme a poltrona que sera reservada (para encerrar digite 0 ou um valor acima da capacidade máxima): ");
-                    scanf("%d", &cancelAssen);
-                }
-            }
-            else if(cancelVoo == voo[2]){
-                printf("\nInforme as poltronas que sera reservadas (para encerrar digite 0 ou um valor acima da capacidade máxima): ");
-                scanf("%d", &cancelAssen);
-                
-                while(cancelAssen > 0){
-                    vagaVoo214[cancelAssen] = 0;
+            printf("\nInforme a poltrona que será cancelada (para encerrar digite 0): ");
+            scanf("%d", &cancelAssen);
 
-                    printf("\nInforme a poltrona que sera reservada (para encerrar digite 0 ou um valor acima da capacidade máxima): ");
-                    scanf("%d", &cancelAssen);
-                }
+            while(cancelAssen > 0){
+                ocupMax[indiceVoo][reserAssen] = 1;
+
+                printf("\nInforme a poltrona que será reservada (para encerrar digite 0): ");
+                scanf("%d", &cancelAssen);
             }
-            else if(cancelVoo == 0){
-                break;
-            }
-            else{
-                printf("Informe um valor válido: ");
-                scanf("%d", &cancelVoo);
-            }
+
             break;
 
         default:
             break;
-        }
+    }
 
     return 0;
 }
